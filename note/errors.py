67825776d2aa9c6d539b906d0a5454693ff349e1
Mohammad Hossein Jadidi <89521814+Mohammadein@users.noreply.github.com>
2026-09-13 10:19:04 +0330

    Refactor error handling in storage and errors modules to streamline file operations

--- note/errors.py
+++ note/errors.py
@@ -1,3 +1,5 @@
+import json
+
 from . import IO
 import functools
 
@@ -15,6 +17,23 @@ def handle_note_errors(func):
             IO.error("Note not found")
         except InvalidInput:
             IO.error("Invalid input")
+        except Exception as e:
+            IO.error(str(e))
+    return wrapper
+
+def handle_file_errors(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except FileNotFoundError:
+            IO.error("File not found")
+        except json.JSONDecodeError:
+            IO.error("Error decoding JSON")
+        except FileExistsError:
+            IO.error("File already exists")
+        except PermissionError:
+            IO.error("Permission denied")
         except Exception as e:
             IO.error(str(e))
     return wrapper
