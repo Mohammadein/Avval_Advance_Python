50d022fc46395eec198d7928fa8a8045939d3b1f
Mohammad Hossein Jadidi <89521814+Mohammadein@users.noreply.github.com>
2026-09-17 12:57:03 +0330

    Add base template and create note form with redirect functionality

--- note/web_app.py
+++ note/web_app.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from note import dependencies
@@ -8,6 +8,9 @@ from note import dependencies
 app = FastAPI()
 template = Jinja2Templates(directory="note/templates")
 
+@app.get("/")
+def home():
+    return RedirectResponse(url="/notes")
 
 @app.get("/notes" , response_class=HTMLResponse)
 def list_notes(request: Request):
@@ -19,3 +22,17 @@ def list_notes(request: Request):
         context={"notes": notes},
         )
 
+@app.get("/notes/new", response_class=HTMLResponse)
+def show_create_note_form(request: Request):
+    return template.TemplateResponse(
+        request=request,
+        name="create_note.html",
+        context={},
+    )
+
+@app.post("/notes")
+def create_note(title: str = Form(), content: str = Form()):
+    note_manager = dependencies.get_note_manager()
+    note_manager.create_note(title, content)
+
+    return RedirectResponse(url="/notes" , status_code=303)
